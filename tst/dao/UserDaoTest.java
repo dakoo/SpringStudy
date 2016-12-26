732e8ed28bb24e8edbd68ea71a3dec7afae35684
dakoo <hochul.shin0@gmail.com>
2016-12-26 13:47:41 -0800

    1.8.2 XML을 이용하는 애플리케이션 컨텍스트

--- tst/dao/UserDaoTest.java
+++ tst/dao/UserDaoTest.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -32,5 +34,21 @@ public class UserDaoTest {
         Assert.assertEquals(1, countingConnectionMaker.getCounter());
         User gotUser = dao.get("1");
         Assert.assertEquals(2, countingConnectionMaker.getCounter());
+    }
+
+    @Test
+    public void testDIUsingGenericXmlApplicationContext() throws ClassNotFoundException, SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("dao/applicationContext.xml");
+        UserDao dao1 = context.getBean("userDao", UserDao.class);
+        UserDao dao2 = context.getBean("userDao", UserDao.class);
+        Assert.assertEquals(dao1, dao2);
+    }
+
+    @Test
+    public void testDIUsingClassPathXMLApplicationContext() throws ClassNotFoundException, SQLException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml", UserDao.class);
+        UserDao dao1 = context.getBean("userDao", UserDao.class);
+        UserDao dao2 = context.getBean("userDao", UserDao.class);
+        Assert.assertEquals(dao1, dao2);
     }
 }
