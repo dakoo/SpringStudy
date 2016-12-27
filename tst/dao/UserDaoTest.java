55ce854895e07a55368a6546eb1209e5f95598e6
dakoo <hochul.shin0@gmail.com>
2016-12-26 19:16:44 -0800

    2.3

--- tst/dao/UserDaoTest.java
+++ tst/dao/UserDaoTest.java
@@ -2,6 +2,7 @@ package dao;
 
 import domain.User;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,31 +14,20 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
 
-    @Test
-    public void testDIUsingGenericXmlApplicationContext() throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("dao/applicationContext.xml");
-        UserDao dao1 = context.getBean("userDao", UserDao.class);
-        UserDao dao2 = context.getBean("userDao", UserDao.class);
-        Assert.assertEquals(dao1, dao2);
-    }
+    private UserDao dao;
 
-    @Test
-    public void testDIUsingClassPathXMLApplicationContext() throws ClassNotFoundException, SQLException {
+    @Before
+    public void setUp() throws Exception {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml", UserDao.class);
-        UserDao dao1 = context.getBean("userDao", UserDao.class);
-        UserDao dao2 = context.getBean("userDao", UserDao.class);
-        Assert.assertEquals(dao1, dao2);
+        dao = context.getBean("userDao", UserDao.class);
+        dao.deleteAll();
     }
 
     @Test
     public void testAddAndGet() throws Exception {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml", UserDao.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        User extractedUser = dao.get("id1");
-        Assert.assertNotNull(extractedUser);
         User user = new User("id1", "name1", "passwd1");
         dao.add(user);
-        extractedUser = dao.get("id1");
+        User extractedUser = dao.get("id1");
         Assert.assertNotNull(extractedUser);
         Assert.assertEquals(user.getId(), extractedUser.getId());
         Assert.assertEquals(user.getName(), extractedUser.getName());
@@ -47,13 +37,9 @@ public class UserDaoTest {
 
     @Test
     public void testCountAndDeleteAll() throws Exception {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml", UserDao.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
         User user1 = new User("id1", "name1", "passwd1");
         User user2 = new User("id2", "name2", "passwd2");
         User user3 = new User("id3", "name3", "passwd3");
-
-        dao.deleteAll();
         Assert.assertEquals(0, dao.getCount());
         dao.add(user1);
         Assert.assertEquals(1, dao.getCount());
@@ -67,9 +53,6 @@ public class UserDaoTest {
 
     @Test(expected = EmptyResultDataAccessException.class)
     public void getUserFailure() throws SQLException {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml", UserDao.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        dao.deleteAll();
         dao.get("id2");
     }
 }
